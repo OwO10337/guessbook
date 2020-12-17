9897c0c765fa54626d69d87a5697385583fc4deb
10337 <10930228@dcsh.tp.edu.tw>
2020-12-17 10:48:37 +0800

    新增專案、路徑規則、處理視團ok

---
+++ web/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import Message
+
+
+# Create your views here.
+class MessageList(ListView):
+    model = Message
+
+class MessageDetail(DetailView):
+    model = Message
+    
+
+
+class Message=CreateView(CreateView):
+    model = Message
+    fields = '__all__'
+    success_url = reverse_lazy('msg_list')
+    
